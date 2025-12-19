c5f6a7c47441021102d51d0259df552626dab495
JohnWickBK99 <johnwick.bk99@gmail.com>
2025-12-19 14:28:11 +0700

    update

--- src/components/OfferList.tsx
+++ src/components/OfferList.tsx
@@ -181,7 +181,7 @@ export default function OfferList({
       <div
         ref={searchBarRef}
         className={cn(
-          'sticky top-[90px] z-10 flex h-fit flex-col items-center justify-between gap-4 px-6 py-4 transition-colors duration-300 sm:flex-row',
+          'sticky top-[90px] z-30 flex h-fit flex-col items-center justify-between gap-4 px-6 py-4 transition-colors duration-300 sm:flex-row',
           'border-b border-border bg-background'
         )}
       >
